65e2668f78ea8a40d041967bbd20365c94488a4e
BryanKeizer <112871567+BryanKeizer@users.noreply.github.com>
2022-11-11 13:42:59 +0100

    Add files via upload
    dit is 8.1 maar dan met de getal te laag

--- opdracht 8.1/app.js
+++ opdracht 8.1/app.js
@@ -4,22 +4,36 @@ let input2 = document.getElementById("getal2");
 
 //maak een functie genaamd add
 function add(){
- let answer = Number(input1.value) + Number(input2.value);
- console.log(answer);
+    if(input1.value <1 || input2.value < 1) {
+        title.innerText ="getal is te laag"
+    }else {
+        let answer = Number(input1.value) + Number(input2.value);
+        title.innerText = answer}
 }
 //maak een functie genaamd multiply
 function multiply(){
+    if(input1.value <1 || input2.value < 1) {
+        title.innerText ="getal is te laag"
+    }else {
     let answer = (input1.value) * (input2.value);
-    console.log(answer);
+    title.innerText = answer
+    }
 }
 //maak een functie genaamd devide
 function devide(){
+    if(input1.value <1 || input2.value < 1) {
+        title.innerText ="getal is te laag"
+    }else {
     let answer = (input1.value) / (input2.value);
-    console.log(answer);
+    title.innerText = answer
+    }
 }
 
 //maak eend functie genaamd minus
 function minus(){
+    if(input1.value <1 || input2.value < 1) {
+        title.innerText ="getal is te laag"
+    }else {
     let answer = (input1.value) - (input2.value);
-    console.log(answer);
+    title.innerText = answer}
 }
